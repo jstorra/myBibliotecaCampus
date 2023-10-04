8f8c13a7016f0377a82f793cd0df560a31bd30c9
jstorra <131458634+jstorra@users.noreply.github.com>
2023-10-03 19:28:44 -0500

    feat: :sparkles: Solicitudes al endpoint categoria
    Se añaden las solicitudes getAll, getOne, post, putOne y deleteOne al endpoint de categoria.

--- functions/solicitudes.js
+++ functions/solicitudes.js
@@ -1,23 +1,26 @@
 import { validacionDato, validacionObj } from "../functions/validaciones.js";
 const config = { headers: { "content-type": "application/json" } };
 export const getAll = async ({uri, endpoint} = {}) => {
-    if (!uri) return { status: 400, message: "Envia la URI por favor."}
-    if (!endpoint) return { status: 400, message: "Envia el Endpoint por favor."}
+    if (!uri) return { status: 400, message: 'Envía la URI por favor.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
     return await (await fetch(`${uri}${endpoint}`)).json();
 } 
 export const getOne = async ({id, uri, endpoint} = {}) => {
-    if (!id) return { status: 400, message: "Envia el Id por favor."}
-    if (typeof id !== 'number') return { status: 400, message: "El Id tiene un formato invalido."}
-    if (!uri) return { status: 400, message: "Envia la URI por favor."}
-    if (!endpoint) return { status: 400, message: "Envia el Endpoint por favor."}
+    if (!id) return { status: 400, message: 'Envía el Id por favor.'}
+    if (typeof id !== 'number') return { status: 400, message: 'El Id tiene un formato inválido.'}
+    if (!uri) return { status: 400, message: 'Envía la URI por favor.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
     let res = await fetch(`${uri}${endpoint}${id}`)
     if (res.status === 404) return { status: res.status, message: res.statusText}
     return await res.json()
 } 
-export const post = async ({obj = {}, tabla, uri, endpoint} = {}) => {
+export const post = async ({obj = {}, tabla = {}, uri, endpoint} = {}) => {
+    if (!uri) return { status: 400, message: 'Envía la URI por favor.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
     let body = {};
     try {
         validacionObj(obj)
+        validacionObj(tabla)
         Object.entries(tabla).forEach((e) => Object.assign(body,validacionDato({nombreCampo: e[0], valor: obj[e[0]], tipoEsperado: e[1]})));
     } catch (error) { return { status: 400, message: error.message }; }
     config.method = "POST";
@@ -25,10 +28,13 @@ export const post = async ({obj = {}, tabla, uri, endpoint} = {}) => {
     let res = await fetch(`${uri}${endpoint}`, config);
     if (res.status === 200 || res.status === 201 || res.status === 404) return { status: res.status, message: res.statusText}
 }
-export const putOne = async ({obj = {}, tabla, uri, endpoint} = {}) => {
+export const putOne = async ({obj = {}, tabla = {}, uri, endpoint} = {}) => {
+    if (!uri) return { status: 400, message: 'Envía la URI por favor.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
     let body = {};
     try {
         validacionObj(obj)
+        validacionObj(tabla)
         validacionDato({nombreCampo: "id", valor: obj.id, tipoEsperado: "number"});
         let data = await getOne({id: obj.id, uri, endpoint})
         obj = {...data, ...obj}
@@ -40,10 +46,10 @@ export const putOne = async ({obj = {}, tabla, uri, endpoint} = {}) => {
     if (res.status === 200 || res.status === 201 || res.status === 404) return { status: res.status, message: res.statusText}
 }
 export const deleteOne = async ({id, uri, endpoint} = {}) => {
-    if (!id) return { status: 400, message: "Envia el Id por favor."}
-    if (typeof id !== 'number') return { status: 400, message: "El Id tiene un formato invalido."}
-    if (!uri) return { status: 400, message: "Envia la URI por favor."}
-    if (!endpoint) return { status: 400, message: "Envia el Endpoint por favor."}
+    if (!id) return { status: 400, message: 'Envía el Id por favor.'}
+    if (typeof id !== 'number') return { status: 400, message: 'El Id tiene un formato inválido.'}
+    if (!uri) return { status: 400, message: 'Envía la URI por favor.'}
+    if (!endpoint) return { status: 400, message: 'Envía el Endpoint por favor.'}
     config.method = "DELETE";
     let res = await fetch(`${uri}${endpoint}${id}`, config);
     if (res.status === 200 || res.status === 404) return { status: res.status, message: res.statusText}
--- storage/categoria.js
+++ storage/categoria.js
@@ -9,9 +9,9 @@ const tabla = {
 // console.log(await deleteOne({id: 1, uri, endpoint}));
 
 // FOR TEST POST
-// const obj = {autorId: 2,categoriaId: 5,editorialId: 2,fechaLanzamiento: "2039-10-02",titulo: "La maquina del tiempo",isbn: "63796",numPaginacion: 100,estadoId: 1};
+// const obj = {nombre: "drama"};
 // console.log(await post({obj, tabla, uri, endpoint}));
 
 // FOR TEST PUT
-// const obj = {id: 1,autorId: 1,categoriaId: 1,editorialId: 1,fechaLanzamiento: "2040-10-02",titulo: "Celestina",isbn: "67890",numPaginacion: 250,estadoId: 1}
+// const obj = {id: 1, nombre: "horror"}
 // console.log(await putOne({obj, tabla, uri, endpoint}));
