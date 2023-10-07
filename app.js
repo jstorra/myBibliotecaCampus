f4fb787adc37cf6f8618dada62a30e73b5a39512
jstorra <131458634+jstorra@users.noreply.github.com>
2023-10-07 12:34:31 -0500

    feat: :sparkles: Incorporación de carga de los libros al iniciar la aplicación.

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 
-import { libroMain } from './views/loadMain.js'
-import libro from './storage/libro.js'
+import { libroMain } from './modules/loadMains.js'
 const content = document.querySelector("#content")
 
 addEventListener("DOMContentLoaded", async ()=> {
-    content.insertAdjacentHTML("beforeend", await libroMain())
-    const form = document.querySelector("form")
-    const inpFecha = document.querySelector("#inpFecha")
-    inpFecha.addEventListener("focus", () => {
-        inpFecha.type="date"
-    })
-    form.addEventListener("submit", async (e)=> {
-        e.preventDefault()
-        const data = Object.fromEntries(new FormData(e.target))
-        Object.keys(data).forEach( async key => {
-            if (key === "numPaginacion") data[key] = Number(data[key])
-            if (key.includes("Id") && !isNaN(data[key].split(".")[0])){
-                data[key] = Number(data[key].split(".")[0])
-            }
-        })
-        let res = await libro.post(data)
-        res.status === 201 ? alert(`!! El libro se ha registrado !!`) : alert(`${res.message}`)
-    })
+    await libroMain(content)
 })
-
