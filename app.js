9c4de4a8b4c4c4267cf2b1f734306e5c8a44d0a1
jstorra <131458634+jstorra@users.noreply.github.com>
2023-10-08 10:13:46 -0500

    refactor: :recycle: Refactorizando codigo
    Se refactoriza el codigo para cargar la pagina de la aplicación una vez el DOM finalice su carga y ademas para moverse entre las demas paginas.

--- app.js
+++ app.js
@@ -2,40 +2,14 @@ import m from "./modules/loadMains.js";
 const content = document.querySelector("#content");
 
 addEventListener("DOMContentLoaded", () => {
-    m.libroMain(content);
-});
-
-document.addEventListener("click", (e) => {
-    if (e.target.matches("#btnLibros")) {
-        document.querySelector("main").remove();
-        m.libroMain(content);
-    }
-    if (e.target.matches("#btnCategorias")) {
-        document.querySelector("main").remove();
-        m.categoriaMain(content);
-    }
-    if (e.target.matches("#btnEditoriales")) {
-        document.querySelector("main").remove();
-        m.editorialMain(content);
-    }
-    if (e.target.matches("#btnEstados")) {
-        document.querySelector("main").remove();
-        m.estadoMain(content);
-    }
-    if (e.target.matches("#btnAutores")) {
-        document.querySelector("main").remove();
-        m.autorMain(content);
-    }
-    if (e.target.matches("#btnUsuarios")) {
-        document.querySelector("main").remove();
-        m.usuarioMain(content);
-    }
-    if (e.target.matches("#btnPrestamos")) {
-        document.querySelector("main").remove();
-        m.prestamoMain(content);
-    }
-    if (e.target.matches("#btnReservas")) {
-        document.querySelector("main").remove();
-        m.reservaMain(content);
-    }
+    let currentPage = "libro"
+    m[`${currentPage}Main`](content)
+    const pages = document.querySelectorAll("a[id]")
+    pages.forEach(btn => {
+        btn.addEventListener("click", () => {
+            document.querySelector("main").remove();
+            currentPage = btn.getAttribute("id").substring(3).toLowerCase()
+            m[`${currentPage}Main`](content)
+        })
+    })
 });
